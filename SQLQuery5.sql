454fc14b2bab1a9df34949bfcbfd7779ede39489
MarinetteTresa <82084418+MarinetteTresa@users.noreply.github.com>
2021-04-17 10:55:18 +1200

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,23 +0,0 @@
-USE [IndustryConnect_DW]
-GO
-
-INSERT INTO [dbo].[DimCustomer]
-           ([CustomerID]
-           ,[CustomerName]
-           ,[TerritoryName]
-           ,[RegionName]
-           ,[ValidFrom]
-           ,[ValidTo])
-     VALUES
-           (<CustomerID, int,>
-           ,<CustomerName, nvarchar(360),>
-           ,<TerritoryName, nvarchar(50),>
-           ,<RegionName, nvarchar(50),>
-           ,<ValidFrom, datetime,>
-           ,<ValidTo, datetime,>)
-GO
-
-
-USE [IndustryConnect_DW]GOINSERT INTO [dbo].[DimCustomer]           ([CustomerID]           ,[CustomerName]           ,[TerritoryName]           ,[RegionName]           )SELECT        AdventureWorks2014.Sales.Customer.CustomerID, AdventureWorks2014.Sales.Store.Name as 'storename', AdventureWorks2014.Sales.SalesTerritory.Name AS 'territory name', AdventureWorks2014.Sales.SalesTerritory.CountryRegionCodeFROM            AdventureWorks2014.sales.Customer INNER JOIN                         AdventureWorks2014.Sales.SalesTerritory ON AdventureWorks2014.Sales.Customer.TerritoryID = AdventureWorks2014.Sales.SalesTerritory.TerritoryID INNER JOIN                         AdventureWorks2014.Sales.Store ON AdventureWorks2014.Sales.Customer.StoreID = AdventureWorks2014.Sales.Store.BusinessEntityID           GO
-
-
